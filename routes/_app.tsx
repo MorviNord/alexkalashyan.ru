d640e082f7cb935ce3d0ecc8ab21de444e607f7e
unknown <morvicool@gmail.com>
2025-06-22 11:38:51 +0300

    update build + schema

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,5 +1,4 @@
 import type { PageProps } from "fresh";
-import { schema } from "../utils/schema.ts";
 
 export default function App({ Component, url }: PageProps) {
   return (
@@ -33,7 +32,23 @@ export default function App({ Component, url }: PageProps) {
         />
         <meta property="og:type" content="website" />
         <script type="application/ld+json">
-          {JSON.stringify(schema).replace(/&quot;/g, '"')}
+          {`{
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "Alexander Kalashyan",
+              "jobTitle": "IT Project Manager",
+              "url": "https://alexkalashyan.ru/",
+              "sameAs": [
+                "https://stavropol.hh.ru/resume/0f989cc3ff0cbfa2c20039ed1f5a38736c3158",
+                "https://github.com/MorviNord"
+              ],
+              "skills": ["Web Development", "Project Management"],
+              "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Stavropol",
+                "addressCountry": "RU"
+              }
+            }`}
         </script>
         <link rel="icon" href="/favicon.ico" />
         <link
