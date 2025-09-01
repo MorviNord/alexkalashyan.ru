25ccb4ecc821f2962a5de716eeaa5256edd5cc0e
unknown <morvicool@gmail.com>
2025-09-01 16:48:32 +0300

    update fresh 2.0 beta

--- main.ts
+++ main.ts
@@ -1,17 +1,9 @@
-import { App, fsRoutes, staticFiles } from "fresh";
+import { App, staticFiles } from "fresh";
 import { define, type State } from "./utils.ts";
 
 export const app = new App<State>();
 
 app.use(staticFiles());
-
-// this is the same as the /api/:name route defined via a file. feel free to delete this!
-app.get("/api2/:name", (ctx) => {
-  const name = ctx.params.name;
-  return new Response(
-    `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`,
-  );
-});
 
 // this can also be defined via a file. feel free to delete this!
 const exampleLoggerMiddleware = define.middleware((ctx) => {
@@ -20,11 +12,5 @@ const exampleLoggerMiddleware = define.middleware((ctx) => {
 });
 app.use(exampleLoggerMiddleware);
 
-await fsRoutes(app, {
-  loadIsland: (path) => import(`./islands/${path}`),
-  loadRoute: (path) => import(`./routes/${path}`),
-});
-
-if (import.meta.main) {
-  await app.listen();
-}
+// Include file-system based routes here
+app.fsRoutes();
